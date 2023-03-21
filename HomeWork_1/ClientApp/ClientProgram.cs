68d49926e35b45257225c813786639d065ed1a50
deus-tll <millerartemka14@gmail.com>
2023-03-22 01:56:08 +0200

    init

---
+++ HomeWork_1/ClientApp/ClientProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientApp
+{
+	internal class ClientProgram
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
---
+++ HomeWork_1/ServerApp/ServerProgram.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace ServerApp
+{
+	internal class ServerProgram
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "Server";
+
+		}
+	}
+
+	internal class MyServer
+	{
+		private int port;
+		private IPAddress ip;
+		private Socket server;
+
+		public Task StartServerAsync() => Task.Run(StartServer);
+
+		public void StartServer()
+		{
+			if (server is not null)
+			{
+				return;
+			}
+
+			server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			IPEndPoint endPoint = new(ip, port);
+
+			try
+			{
+				server.Bind(endPoint);
+
+				server.Listen();
+				Console.WriteLine("Server started, accept connection...");
+
+
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+	}
+}
